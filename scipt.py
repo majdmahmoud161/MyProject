033dc961a0cf081493d4b03cd7add6506532df66
majd mahmoud <majd.mahmoud161@gmail.com>
2025-12-31 22:22:46 +0300

    nothing at all

---
+++ scipt.py
@@ -0,0 +1,54 @@
+# print statment
+print("hellow",12)
+print(60)
+# ..................................
+
+# variable
+name='majd'
+age=24
+print(age) 
+# ...................................
+
+# input
+name=input('name: ')
+age=input('age:')
+print(name,age)
+# ...................................
+
+# arrithmetic operations
+x=10
+y=5
+res=int(x/y)
+res=x ** y
+res=x//y
+print(res)
+# ...................................
+
+# String methode
+name='majd'.lower()
+name='majd'.capitalize()
+
+print(name)
+
+# ..................................
+
+# conditional opperations
+x='hellow'
+y='hellow'
+print(x==y)
+print(4>5)
+
+print(not False)
+x=10
+if(x==10):
+    print(True)
+    print("x is 10")
+array=['majd',1,True,False]
+array.append('new item')
+print(array)
+array2=(0,1,2,3)
+
+print(array2)
+
+s={1,1,2,2,3,3}
+print(s)
